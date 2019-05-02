3e43cba654cb1f08921dc5b9b9ca0469088af73c
nohseonghun <jzi040941@naver.com>
2019-05-02 16:34:25 +0900

    test

---
+++ build/CMakeFiles/purePlayTest.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jzi04094/git/purePlayTest/main.cpp" "/home/jzi04094/git/purePlayTest/build/CMakeFiles/purePlayTest.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/purePlayTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/purePlayTest.dir/main.cpp.o"
+  "purePlayTest.pdb"
+  "purePlayTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/purePlayTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
